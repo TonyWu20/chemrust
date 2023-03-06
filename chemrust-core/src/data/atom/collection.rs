90db073d89162a5020199162d6ba6f3d5889a928
TonyWu20 <tony.w21@gmail.com>
2023-03-06 11:29:38 +0800

    Design the `AtomCollections` as SoA for data manipulation. Implement the conversion between `Vec<Atom>` and `AtomCollections`

--- chemrust-core/src/data/atom/collection.rs
+++ chemrust-core/src/data/atom/collection.rs
@@ -1,80 +1,84 @@
-use std::fmt::Debug;
+use nalgebra::Point3;
 
-use crate::system::data_builder::Pending;
-use crate::system::{data_builder::AtomCollectionBuilder, data_view::AttrCollectionView};
+use super::Atom;
 
-use crate::data::format::DataFormat;
-
-use super::{
-    AtomAttr, AtomAttrMarker, AtomId, AtomicNumber, CartesianCoord, ElementSymbol, FractionalCoord,
-};
-#[derive(Debug, Clone)]
-pub struct AtomCollection<T: DataFormat> {
-    pub(crate) element_symbols: Vec<ElementSymbol<T>>,
-    pub(crate) atomic_number: Vec<AtomicNumber<T>>,
-    pub(crate) xyz: Vec<CartesianCoord<T>>,
-    pub(crate) fractional_xyz: Option<Vec<FractionalCoord<T>>>,
-    pub(crate) atom_ids: Vec<AtomId<T>>,
-    pub(crate) size: usize,
+#[derive(Debug, PartialEq, PartialOrd)]
+pub struct AtomCollections {
+    symbols: Vec<String>,
+    atomic_numbers: Vec<u8>,
+    cartesian_coords: Vec<Point3<f64>>,
+    indexes: Vec<usize>,
+    size: usize,
 }
 
-impl<T: DataFormat> AtomCollection<T> {
-    pub fn builder(size: usize) -> AtomCollectionBuilder<T, Pending> {
-        AtomCollectionBuilder::<T, Pending>::new(size)
-    }
-    pub fn element_symbols(&self) -> &[ElementSymbol<T>] {
-        self.element_symbols.as_ref()
-    }
-
-    pub fn atomic_number(&self) -> &[AtomicNumber<T>] {
-        self.atomic_number.as_ref()
-    }
-
-    pub fn xyz(&self) -> &[CartesianCoord<T>] {
-        self.xyz.as_ref()
-    }
-
-    pub fn fractional_xyz(&self) -> Option<&Vec<FractionalCoord<T>>> {
-        self.fractional_xyz.as_ref()
-    }
-
-    pub fn atom_ids(&self) -> &[AtomId<T>] {
-        self.atom_ids.as_ref()
-    }
-
-    pub fn size(&self) -> usize {
-        self.size
-    }
-}
-
-impl<T, N, F, U, M> AttrCollectionView<Vec<AtomAttr<U, M, F>>> for Vec<AtomAttr<T, N, F>>
-where
-    T: PartialEq + Clone,
-    N: AtomAttrMarker,
-    F: DataFormat,
-    U: PartialEq + Clone,
-    M: AtomAttrMarker,
-{
-    type Ref = Vec<AtomAttr<U, M, F>>;
-    type Output = T;
-    type NewCollection = Self;
-
-    fn view_content_at(&self, index: usize) -> Option<&Self::Output> {
-        Some(self.get(index).as_ref()?.content())
-    }
-
-    fn rearrange_with(self, other: &Self::Ref) -> Self::NewCollection {
-        let mut new_vec: Vec<AtomAttr<T, N, F>> = Vec::with_capacity(self.len());
-        for item in other.iter() {
-            let reference_index: &usize = item.index();
-            let mapped_item = &self[*reference_index];
-            new_vec.push(mapped_item.clone())
+impl AtomCollections {
+    pub fn new(size: usize) -> Self {
+        AtomCollections {
+            symbols: Vec::with_capacity(size),
+            atomic_numbers: Vec::with_capacity(size),
+            cartesian_coords: Vec::with_capacity(size),
+            indexes: Vec::with_capacity(size),
+            size,
         }
-        new_vec
     }
-    fn update_all_index(&mut self) {
-        self.iter_mut()
-            .enumerate()
-            .for_each(|(i, item)| item.set_index(i));
+    pub fn symbols(&self) -> &[String] {
+        self.symbols.as_ref()
+    }
+
+    pub fn atomic_numbers(&self) -> &[u8] {
+        self.atomic_numbers.as_ref()
+    }
+
+    pub fn cartesian_coords(&self) -> &[Point3<f64>] {
+        self.cartesian_coords.as_ref()
+    }
+
+    pub fn indexes(&self) -> &[usize] {
+        self.indexes.as_ref()
+    }
+    /// Retrieve an `Atom` at given 0th-based index.
+    pub fn get_atom_at(&self, index: usize) -> Option<Atom> {
+        if index >= self.size {
+            None
+        } else {
+            let symbol = self.symbols().get(index).unwrap();
+            let atomic_num = self.atomic_numbers().get(index).unwrap();
+            let cartesian_coord = self.cartesian_coords().get(index).unwrap();
+            Some(
+                Atom::new_builder()
+                    .with_symbol(symbol)
+                    .with_atomic_number(*atomic_num)
+                    .with_coord(cartesian_coord)
+                    .with_index(index)
+                    .finish()
+                    .unwrap()
+                    .build(),
+            )
+        }
+    }
+}
+
+impl From<Vec<Atom>> for AtomCollections {
+    fn from(value: Vec<Atom>) -> Self {
+        let collection_size = value.len();
+        let mut collections = AtomCollections::new(collection_size);
+        value.iter().for_each(|atom| {
+            collections.symbols.push(atom.symbol().into());
+            collections.atomic_numbers.push(atom.atomic_number());
+            collections.cartesian_coords.push(atom.cartesian_coord());
+            collections.indexes.push(atom.index);
+        });
+        collections
+    }
+}
+
+impl From<AtomCollections> for Vec<Atom> {
+    fn from(value: AtomCollections) -> Self {
+        let collection_size = value.indexes().len();
+        let mut atom_vec: Vec<Atom> = Vec::with_capacity(collection_size);
+        for i in 0..collection_size {
+            atom_vec.push(value.get_atom_at(i).unwrap())
+        }
+        atom_vec
     }
 }

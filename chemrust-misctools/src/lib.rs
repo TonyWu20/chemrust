06acbe482a34b65c7507378715c067673617ec15
TonyWu20 <tony.w21@gmail.com>
2023-07-21 19:47:51 +0800

    Add `to_xsd_scripts` to `chemrust-misctools`

--- chemrust-misctools/src/lib.rs
+++ chemrust-misctools/src/lib.rs
@@ -1,4 +1,6 @@
+use glob::glob;
 use std::{
+    error::Error,
     fs::{self, read_to_string},
     io,
     path::Path,
@@ -15,12 +17,12 @@ pub fn write_lsf_script<P: AsRef<Path>>(cell_path: &P, num_nodes: u32) -> Result
     let prefix = format!(
         r#"APP_NAME=intelY_mid
 NP={}
-NP_PER_NODE={}
+NP_PER_NODE=12
 OMP_NUM_THREADS=1
 RUN="RAW"
 
 "#,
-        num_nodes, num_nodes
+        num_nodes,
     );
     let content = format!("{prefix}{cmd}");
     let lsf_filepath = target_dir.join("MS70_YW_CASTEP.lsf");
@@ -45,4 +47,51 @@ pub fn copy_potentials<P: AsRef<Path>>(cell_path: &P, potential_loc: &P) -> Resu
                 Ok(())
             }
         })
+}
+
+/// Scan the generated `msi` files, create a perl script to be run in `Materials Studio`
+/// to save as `xsd` format.
+pub fn to_xsd_scripts(target_root_dir: &str) -> Result<(), Box<dyn Error>> {
+    let msi_pattern = format!("{target_root_dir}/**/*.msi");
+    let item_collection = glob(&msi_pattern)
+        .expect("Failed to read glob pattern")
+        .into_iter()
+        .map(|entry| -> Option<String> {
+            match entry {
+                Ok(path) => {
+                    let stem = path.file_stem().unwrap();
+                    let parent = path.parent().unwrap();
+                    Some(format!(
+                        r#""{}/{}""#,
+                        parent.to_str().unwrap(),
+                        stem.to_str().unwrap()
+                    ))
+                }
+                Err(e) => {
+                    println!("glob entry match error: {:?}", e);
+                    None
+                }
+            }
+        })
+        .collect::<Vec<Option<String>>>()
+        .iter()
+        .map(|entry| -> String { entry.as_ref().unwrap().to_string() })
+        .collect::<Vec<String>>();
+    let all_files_text = item_collection.join(", ");
+    let headlines = r#"#!perl
+use strict;
+use Getopt::Long;
+use MaterialsScript qw(:all);
+"#;
+    let array_text = format!("my @params = (\n{});\n", all_files_text);
+    let actions = r#"foreach my $item (@params) {
+    my $doc = $Documents{"${item}.msi"};
+    $doc->CalculateBonds;
+    $doc->Export("${item}.xsd");
+    $doc->Save;
+    $doc->Close;
+}"#;
+    let contents = format!("{headlines}{array_text}{actions}");
+    fs::write(Path::new("msi_to_xsd.pl"), contents)?;
+    Ok(())
 }

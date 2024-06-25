b71789dd7a7934b94a91cd5bf8b88358641b53c9
TonyWu20 <tony.w21@gmail.com>
2024-06-25 17:16:25 +0800

    Try to add trait `symmetry info`

--- chemrust-core/src/data/lattice/crystal/mod.rs
+++ chemrust-core/src/data/lattice/crystal/mod.rs
@@ -12,55 +12,8 @@ pub trait CrystalModel {
     fn get_atom_data_mut(&mut self) -> &mut Self::AtomData;
 }
 
-// /// Basic example of a crystal model
-// #[derive(Debug, Clone)]
-// pub struct LatticeCell {
-//     lattice_param: LatticeVectors,
-//     atoms: Atoms,
-// }
-
-// impl LatticeCell {
-//     pub fn new(lattice_param: LatticeVectors, atoms: Atoms) -> Self {
-//         Self {
-//             lattice_param,
-//             atoms,
-//         }
-//     }
-
-//     pub fn set_atoms(&mut self, atoms: Atoms) {
-//         self.atoms = atoms;
-//     }
-
-//     pub fn atoms_mut(&mut self) -> &mut Atoms {
-//         &mut self.atoms
-//     }
-
-//     pub fn lattice_param(&self) -> LatticeVectors {
-//         self.lattice_param
-//     }
-
-//     pub fn set_lattice_param(&mut self, lattice_param: LatticeVectors) {
-//         self.lattice_param = lattice_param;
-//     }
-// }
-
-// impl CrystalModel for LatticeCell {
-//     type AtomData = Atoms;
-//     type LatticeData = LatticeVectors;
-
-//     fn get_cell_parameters(&self) -> &Self::LatticeData {
-//         &self.lattice_param
-//     }
-
-//     fn get_atom_data(&self) -> &Self::AtomData {
-//         &self.atoms
-//     }
-
-//     fn get_cell_parameters_mut(&mut self) -> &mut Self::LatticeData {
-//         &mut self.lattice_param
-//     }
-
-//     fn get_atom_data_mut(&mut self) -> &mut Self::AtomData {
-//         &mut self.atoms
-//     }
-// }
+pub trait SymmetryInfo {
+    fn get_space_group_it_num<T: Fn(&Self) -> u8>(&self, f: T) -> u8 {
+        f(self)
+    }
+}

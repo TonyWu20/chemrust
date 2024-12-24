c9d06f167331aed167be3550f4d7438a9921c7da
TonyWu20 <tony.w21@gmail.com>
2024-12-24 17:43:36 +0800

    Breaking changes: the `CrystalModel` trait no longer needs associated types

--- chemrust-core/src/data/lattice/crystal/mod.rs
+++ chemrust-core/src/data/lattice/crystal/mod.rs
@@ -4,10 +4,8 @@ use crate::data::lattice::cell_param::UnitCellParameters;
 
 /// The struct to represent a crystal model structure should implement this trait.
 pub trait CrystalModel {
-    type LatticeData: UnitCellParameters;
-    type AtomData: CoreAtomData;
-    fn get_cell_parameters(&self) -> &Self::LatticeData;
-    fn get_atom_data(&self) -> &Self::AtomData;
-    fn get_cell_parameters_mut(&mut self) -> &mut Self::LatticeData;
-    fn get_atom_data_mut(&mut self) -> &mut Self::AtomData;
+    fn get_cell_parameters(&self) -> &impl UnitCellParameters;
+    fn get_atom_data(&self) -> &impl CoreAtomData;
+    fn get_cell_parameters_mut(&mut self) -> &mut impl UnitCellParameters;
+    fn get_atom_data_mut(&mut self) -> &mut impl CoreAtomData;
 }
